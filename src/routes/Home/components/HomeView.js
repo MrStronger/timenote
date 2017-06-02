6326af07cae7572b2e484cf6631d4d556cf815b5
zed31124 <1532680685@qq.com>
2017-06-02 12:26:49 +0800

    测试开发分支

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -5,6 +5,7 @@ import style from './HomeView.scss'
 export const HomeView = () => (
   <div>
     <h4>This is a duck, because Redux!</h4>
+    <span>测试开发分支</span>
     <img className={style.duck} src={DuckImage} />
   </div>
 )

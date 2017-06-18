9d20c3bc12db818262ec6ac734247bc7246966f6
MrStronger <1274552194@qq.com>
2017-06-18 16:12:59 +0800

    [fix]修复了Toggle点击样式问题;添加了tools工具库

--- src/routes/Follow/components/ArticleToggle.js
+++ src/routes/Follow/components/ArticleToggle.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { siblings } from '../../../tools/DOMmethods'
 import PropTypes from 'prop-types'
 import './styles/ArticleToggle.scss'
 
@@ -6,6 +7,15 @@ export default class ArticleToggle extends Component {
   static propTypes = {
     sign: PropTypes.bool.isRequired,
     changeToggle: PropTypes.func.isRequired
+  }
+  handleClick (e) {
+    let target = e.target
+    if (target && target.parentNode && target.nodeName.toUpperCase() === 'LI') {
+      target.classList.add('li-active')
+      siblings(target).forEach((item) => {
+        item.classList.remove('li-active')
+      })
+    }
   }
   render () {
     const { sign = false, changeToggle } = this.props
@@ -17,10 +27,10 @@ export default class ArticleToggle extends Component {
       ]
     }
     return (
-      <ul className='note-trigger list-inline'>
-        <li onClick={() => changeToggle('new')}><a><i className='fa fa-paw' />最新动态</a></li>
-        <li onClick={() => changeToggle('notes')}><a><i className='fa fa-book' />Ta的笔记</a></li>
-        <li onClick={() => changeToggle('hot')}><a><i className='fa fa-fire' />热门</a></li>
+      <ul className='note-trigger list-inline' onClick={(e) => this.handleClick(e)}>
+        <li onClick={() => changeToggle('new')} className='li-active'><i className='fa fa-paw' />最新动态</li>
+        <li onClick={() => changeToggle('notes')}><i className='fa fa-book' />Ta的笔记</li>
+        <li onClick={() => changeToggle('hot')}><i className='fa fa-fire' />热门</li>
         {rest}
       </ul>
     )
